207e27a92f6d63a92ed78e765cda1f99c40b0141
pota-dra <19222929+pota-dra@users.noreply.github.com>
2026-01-04 11:58:25 +0900

    up

---
+++ plugins/CORE/Ex/ExStateIcon.js
@@ -0,0 +1,83 @@
+/*:
+@plugindesc
+ステートアイコン参照制御文字 Ver1.0.0(2026/1/3)
+
+@url https://raw.githubusercontent.com/pota-gon/RPGMakerMZ/refs/heads/main/plugins/BASIC/Ex/ExStateIcon.js
+@target MZ
+@author ポテトードラゴン
+
+・アップデート情報
+* Ver1.0.0: 初期版完成
+
+Copyright (c) 2026 ポテトードラゴン
+Released under the MIT License.
+https://opensource.org/license/mit
+
+@help
+## 概要
+ステート名を参照する制御文字 \SSI を追加します
+
+## 使い方
+\SSI[毒] のようにステート名を記載すると  
+[アイコン] のようにアイコンが表示されるようになります
+
+同じステート名がある場合は、最初に見つけたステートアイコンが表示されます
+*/
+(() => {
+    'use strict';
+
+    // ベースプラグインの処理
+
+
+
+    function Potadra_search(data, id, column = "name", search_column = "id", val = "", initial = 1) {
+        if (id === null || id === undefined) return val;
+        let cache = Potadra__searchCache_get(data);
+        if (!cache) {
+            cache = {};
+            Potadra__searchCache_set(data, cache);
+        }
+        const key = `${search_column}:${id}`;
+        if (key in cache) {
+            const entry = cache[key];
+            return column ? entry?.[column] ?? val : entry;
+        }
+        let result = val;
+        for (let i = initial; i < data.length; i++) {
+            const item = data[i];
+            if (!item) continue;
+            if (search_column && item[search_column] == id) {
+                result = column ? item[column] : item;
+                cache[key] = item;
+                return result;
+            }
+            if (!search_column && i == id) {
+                result = column ? item[column] : item;
+                cache[key] = item;
+                return result;
+            }
+        }
+        cache[key] = val;
+        return val;
+    }
+    function Potadra_nameSearch(data, name, column = "id", search_column = "name", val = "", initial = 1) {
+        return Potadra_search(data, name, column, search_column, val, initial);
+    }
+
+    /**
+     * 制御文字の事前変換
+     *    実際の描画を始める前に、原則として文字列に変わるものだけを置き換える。
+     *    文字「\」はエスケープ文字（\e）に変換。
+     *
+     * @param {} text - 
+     * @returns {} 
+     */
+    const _Window_Base_convertEscapeCharacters = Window_Base.prototype.convertEscapeCharacters;
+    Window_Base.prototype.convertEscapeCharacters = function(text) {
+        let tmp_text = _Window_Base_convertEscapeCharacters.apply(this, arguments);
+        tmp_text = tmp_text.replace(/\x1bSSI\[(.+?)\]/gi, (_, p1) =>
+            "\x1bI[" + Potadra_nameSearch($dataStates, p1, 'iconIndex') + "]"
+        );
+        return tmp_text;
+    };
+})();
